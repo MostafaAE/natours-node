c41bd176af7faa44f3597d7d20d329a6c2a7d65a
MostafaAE <mostafa.aymanae@gmail.com>
2023-10-05 00:53:35 +0300

    Implement pre save hook (middleware) to add slug to tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const { default: slugify } = require('slugify');
+
+const slug = require('slugify');
 
 // tour schema
 const tourSchema = mongoose.Schema(
@@ -7,6 +10,9 @@ const tourSchema = mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true
+    },
+    slug: {
+      type: String
     },
     duration: {
       type: Number,
@@ -35,12 +41,12 @@ const tourSchema = mongoose.Schema(
     discount: Number,
     summary: {
       type: String,
-      trim: true
+      trim: true,
+      required: [true, 'A tour must have a summary']
     },
     description: {
       type: String,
-      trim: true,
-      required: [true, 'A tour must have a description']
+      trim: true
     },
     imageCover: {
       type: String,
@@ -62,6 +68,17 @@ const tourSchema = mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: runs before the .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 // tour model
 const Tour = mongoose.model('Tour', tourSchema);
