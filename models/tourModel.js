2e9d9fc4eb1cee58370c7c77eb25a70b49fbf97a
MostafaAE <mostafa.aymanae@gmail.com>
2023-10-05 01:25:39 +0300

    Implement pre find hook (middleware) to hide secret tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -57,7 +57,11 @@ const tourSchema = mongoose.Schema(
       type: Date,
       default: Date.now()
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -79,6 +83,21 @@ tourSchema.pre('save', function(next) {
 //   console.log(doc);
 //   next();
 // });
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took: ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
+  next();
+});
 
 // tour model
 const Tour = mongoose.model('Tour', tourSchema);
