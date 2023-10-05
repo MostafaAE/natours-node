9a39ac526fd0af1609231497038be782b8befd33
MostafaAE <mostafa.aymanae@gmail.com>
2023-10-05 04:17:54 +0300

    Handle unhandled routs

--- app.js
+++ app.js
@@ -16,4 +16,11 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
+
 module.exports = app;
