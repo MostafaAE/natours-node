521a0dac85c9c2e528f1891aabd905badd06e785
MostafaAE <mostafa.aymanae@gmail.com>
2023-10-06 06:19:25 +0300

    Handle duplicate database fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,6 +6,12 @@ const handleCastErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = err => {
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -43,7 +49,9 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
-    if (err.name === 'CastError') error = handleCastErrorDB(error);
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     sendErrorProd(error, res);
   }
 };
