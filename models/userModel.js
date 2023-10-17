f2b157400d056b67bc0efc39de8667ae8b9c0273
MostafaAE <mostafa.aymanae@gmail.com>
2023-10-17 05:32:11 +0300

    Implement protect routes middleware

--- models/userModel.js
+++ models/userModel.js
@@ -35,7 +35,8 @@ const userSchema = mongoose.Schema({
       },
       message: 'Passwords do not match'
     }
-  }
+  },
+  passwordChangedAt: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -56,6 +57,18 @@ userSchema.methods.correctPassword = async function(
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
