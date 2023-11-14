b5f23c1fbb97e79d24636c3d697eeda8964e1af1
MostafaAE <mostafa.aymanae@gmail.com>
2023-11-14 22:49:44 +0200

    Implement credit card payment with stripe

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 const viewRouter = require('./routes/viewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 
 const app = express();
 
@@ -72,6 +73,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 // Unhandled routes handler middleware
 app.all('*', (req, res, next) => {
